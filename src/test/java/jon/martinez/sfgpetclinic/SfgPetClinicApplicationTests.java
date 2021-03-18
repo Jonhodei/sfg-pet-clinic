9563a505910f414b43a61e3059a5843990d35126
Jonhodei <jonhodei27@gmail.com>
2021-03-18 15:25:06 +0100

    Initial Project Add

---
+++ src/test/java/jon/martinez/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package jon.martinez.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
