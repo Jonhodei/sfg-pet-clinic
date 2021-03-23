aa4260f563e80bfe1a37da57709d02eff8661e08
Jonhodei <jonhodei27@gmail.com>
2021-03-23 09:34:06 +0100

    Impl Map based Services

---
+++ pet-clinic-data/src/main/java/jon/martinez/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package jon.martinez.sfgpetclinic.services.map;
+
+import jon.martinez.sfgpetclinic.model.Pet;
+import jon.martinez.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
